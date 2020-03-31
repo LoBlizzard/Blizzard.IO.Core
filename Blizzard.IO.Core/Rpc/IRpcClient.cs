dfd162abe0a8be175d4a437bea1406c295bc1e30
ybs007 <yair0506474781@gmail.com>
2020-03-31 18:54:09 +0300

    Update Blizzard.IO.Core/Rpc/IRpcClient.cs
    Co-Authored-By: alloncm <alloncm@gmail.com>

--- Blizzard.IO.Core/Rpc/IRpcClient.cs
+++ Blizzard.IO.Core/Rpc/IRpcClient.cs
@@ -4,7 +4,7 @@ namespace Blizzard.IO.Core.Rpc
 {
     public interface IRpcClient
     {
-        TRes Request<TReq, TRes>(TReq request);
-        Task<TRes> RequestASync<TReq, TRes>(TReq request);
+        TRespond Request<TRequest , TRespond>(TRequest request);
+        Task<TRespond> RequestASync<TRequest , TRespond>(TRequest request);
     }
 }
