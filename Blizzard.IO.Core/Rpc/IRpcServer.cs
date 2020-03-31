9487f8daf0b85c660a7d3b3531ade99f9297086b
Yair Ben Simon <yair0506474781@gmai.com>
2020-03-31 13:49:56 +0300

    Add IRpcServer

---
+++ Blizzard.IO.Core/Rpc/IRpcServer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Blizzard.IO.Core.Rpc
+{
+    public interface IRpcServer
+    {
+        void Respond<TReq, TRes>(Func<TReq, TRes> callback);
+        void RespondAsync<TReq, TRes>(Func<TReq, Task<TRes>> callback);
+    }
+}
