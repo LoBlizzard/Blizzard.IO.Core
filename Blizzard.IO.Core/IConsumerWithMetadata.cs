64a451f4e550632363426fff22deb7f98a746fea
Yair Ben Simon <yair0506474781@walla.co.il>
2020-03-31 13:32:12 +0300

    Add IConsumerWithMetadata

---
+++ Blizzard.IO.Core/IConsumerWithMetadata.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Blizzard.IO.Core
+{
+    public interface IConsumerWithMetadata<TData, TMetadata> where TMetadata : BaseMetadata
+    {
+        event Action<TData, TMetadata> MessageWithMetadataConsumed;
+    }
+}
