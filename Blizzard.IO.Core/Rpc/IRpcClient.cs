751a449f9e23f06aa660f431d9f41acdec663cbe
Yair Ben Simon <yair0506474781@gmai.com>
2020-03-31 13:50:04 +0300

    Add IRpcClient

---
+++ Blizzard.IO.Core/Rpc/IRpcClient.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Blizzard.IO.Core.Rpc
+{
+    public interface IRpcClient
+    {
+        TRes Request<TReq, TRes>(TReq request);
+        Task<TRes> RequestASync<TReq, TRes>(TReq request);
+    }
+}
